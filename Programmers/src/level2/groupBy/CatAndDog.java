bcfa23caf0f7fc59bc442210aa47d03db374d277
dayo94 <joungaejs4@naver.com>
2022-01-02 22:49:13 +0900

    고양이와 개는 몇 마리 있을까

--- Programmers/src/level2/groupBy/CatAndDog.java
+++ Programmers/src/level2/groupBy/CatAndDog.java
@@ -4,7 +4,16 @@ public class CatAndDog {
 
 	public static void main(String[] args) {
 
-		System.out.println("한글되나요?");
-	}
+		/*
+		 * 프로그래머스 고양이와 개 쿼리문
+		 * 
+		 * -- 코드를 입력하세요 
+		 * SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) count 
+		 * FROM ANIMAL_INS
+		 * GROUP BY ANIMAL_TYPE 
+		 * ORDER BY NAME DESC;
+		 */
+		
+	}//main
 
-}
+}//class
