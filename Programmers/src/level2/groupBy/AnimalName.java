513161adc4b49a83d84c358a6cccab9d98df8f91
dayo94 <joungaejs4@naver.com>
2022-01-02 22:52:03 +0900

    동명 동물 수  찾기

---
+++ Programmers/src/level2/groupBy/AnimalName.java
@@ -0,0 +1,20 @@
+package level2.groupBy;
+
+public class AnimalName {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 프로그래머스 동명 동물 수 찾기 쿼리문
+		 * 
+		 * -- 코드를 입력하세요 
+		 * SELECT NAME, COUNT(NAME) count 
+		 * FROM ANIMAL_INS 
+		 * GROUP BY NAME
+		 * HAVING count > 1 
+		 * ORDER BY NAME;
+		 */
+		
+	}//main
+
+}//class
